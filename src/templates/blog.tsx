242b2957fcc4098016833da294a22da314efd5cb
Roman Mühlfeldner <roman@muehlfeldner.net>
2020-01-17 12:13:19 +0100

    Generate individual blog page content

--- src/templates/blog.tsx
+++ src/templates/blog.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
-  return <Layout>This is blog template</Layout>
+export const query = graphql`
+  query($slug: String) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = (props: any) => {
+  return (
+    <Layout>
+      <h2>{props.data.markdownRemark.frontmatter.title}</h2>
+      <em>{props.data.markdownRemark.frontmatter.date}</em>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      ></div>
+    </Layout>
+  )
 }
 
 export default Blog
