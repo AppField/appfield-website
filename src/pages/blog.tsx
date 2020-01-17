3594f96e2b867f36cc6dda04e63106959cd980c4
Roman Mühlfeldner <roman@muehlfeldner.net>
2020-01-17 11:23:17 +0100

    Added dynamic blog list

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,37 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+            }
+            excerpt
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h2>Blog</h2>
-      <p>Posts will show up here later on.</p>
+      {data.allMarkdownRemark.edges.map((edge: any) => {
+        return (
+          <div key={edge.node.id}>
+            <h1>{edge.node.frontmatter.title} </h1>
+            <em>{edge.node.frontmatter.date}</em>
+            <p>{edge.node.excerpt}...</p>
+          </div>
+        )
+      })}
     </Layout>
   )
 }
